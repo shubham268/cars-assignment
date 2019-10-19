c53bbe5d41605695d4f9b44516f1c43d3fde9014
shubham268 <shubhamrawat.140@gmail.com>
2019-10-19 12:18:50 +0530

    Update resolver.component.ts

--- src/app/resolver/resolver.component.ts
+++ src/app/resolver/resolver.component.ts
@@ -2,8 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-resolver',
-  templateUrl: './resolver.component.html',
-  styleUrls: ['./resolver.component.css']
+  templateUrl: './resolver.component.html'
 })
 export class ResolverComponent implements OnInit {
 
